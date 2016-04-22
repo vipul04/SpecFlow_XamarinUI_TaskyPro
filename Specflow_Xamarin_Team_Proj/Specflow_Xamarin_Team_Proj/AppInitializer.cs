462d4d4f27d13a42b2a2abb38f392a59973811db
Chris Gungaloo <cgungaloo@qaworks.com>
2016-04-22 10:25:19 +0100

    Added empty project with nunit, xamarin uiTest and Specflow nuget packages

---
+++ Specflow_Xamarin_Team_Proj/Specflow_Xamarin_Team_Proj/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Specflow_Xamarin_Team_Proj
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
